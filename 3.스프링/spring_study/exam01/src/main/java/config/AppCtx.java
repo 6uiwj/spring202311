2d5648238340afb1fd9d1ed44af1b88ef171276b
6uiwj <oterges00@gmail.com>
2023-12-22 17:53:40 +0900

    231222

--- 3.스프링/spring_study/exam01/src/main/java/config/AppCtx.java
+++ 3.스프링/spring_study/exam01/src/main/java/config/AppCtx.java
@@ -4,11 +4,12 @@ import models.Greeter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration  //스프링 설정 클래스
 public class AppCtx {
 
-    @Bean
+    @Bean //스프링 컨테이너 관리 객체
     public Greeter greeter(){
+
         return new Greeter();
     }
 }
--- 3.스프링/spring_study/exam01/src/main/java/main/Ex01.java
+++ 3.스프링/spring_study/exam01/src/main/java/main/Ex01.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Ex01 {
     public static void main(String[] args) {
+        //스프링 컨테이너 객체 생성
+        //AppCtx: 설정파일 -> 관리할 객체와 의존주입 대상 정보
         AnnotationConfigApplicationContext ctx = new
                 AnnotationConfigApplicationContext(AppCtx.class);
 
+        //Bean의 이름: 메서드명
+        //getBean(빈의 이름, 빈의 타입) : 필요한 객체를 찾아옴
         Greeter g1 = ctx.getBean("greeter", Greeter.class);
         g1.hello("이이름");
 

912b85cb90824fe4ed01b407dca14ddd48bbae67
6uiwj <oterges00@gmail.com>
2023-12-26 17:02:41 +0900

    231226 -1

--- 4.자바보강/ioex/src/exam02/Ex03.java
+++ 4.자바보강/ioex/src/exam02/Ex03.java
@@ -4,15 +4,19 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Ex03 {
+    /*
     public static void main(String[] args) {
-        try(FileOutputStream for = new FileOutputStream("test3.txt")) {
+        try(FileOutputStream fos = new FileOutputStream("test3.txt")) {
             fos.write('안');
             fos.write('녕');
             fos.write('하');
             fos.write('세');
             fos.write('요');
+            fos.write(',');
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+     */
 }
